510cbc2992da4fbe100800f3c1785c3ae4b14236
ProgramandoComAndre <andrerafael112@gmail.com>
2021-02-10 16:20:47 +0000

    Aula1

---
+++ Aula1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1, num2, resultado;
+
+            Console.WriteLine("Numero1: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero2: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            resultado = num1 + num2;
+
+            Console.WriteLine("Soma: {0}", resultado);
+            Console.ReadKey();
+        }
+    }
+}
