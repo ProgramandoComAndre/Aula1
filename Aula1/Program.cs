14bd818043513ecbfa78ab882e5527a518602d0b
ProgramandoComAndre <andrerafael112@gmail.com>
2021-02-10 16:28:37 +0000

    Aula1 comentarios

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -7,17 +7,21 @@ namespace Aula1
     {
         static void Main()
         {
+            //variaveis
             int num1, num2, resultado;
 
+            //leitura dos numeros
             Console.WriteLine("Numero1: ");
             num1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Numero2: ");
             num2 = int.Parse(Console.ReadLine());
 
+            //soma dos numeros
             resultado = num1 + num2;
 
-            Console.WriteLine("Soma: {0}", resultado);
+            //apresentacao dos resultados
+            Console.WriteLine("Soma: {0}", resultado); 
             Console.ReadKey();
         }
     }
